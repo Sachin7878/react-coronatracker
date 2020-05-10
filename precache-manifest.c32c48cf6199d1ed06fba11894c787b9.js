b0d5d0987dcb82e9ba5144ac13d54a25be754240
Sachin <sheelavantsachin@gmail.com>
2020-05-10 13:34:48 +0530

    Updates

---
+++ precache-manifest.c32c48cf6199d1ed06fba11894c787b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3f9fc7fe37366222507c138f065e667",
+    "url": "/react-coronatracker/index.html"
+  },
+  {
+    "revision": "6b9747e988af24490f75",
+    "url": "/react-coronatracker/static/css/main.2f099a18.chunk.css"
+  },
+  {
+    "revision": "d0297c96433716bee866",
+    "url": "/react-coronatracker/static/js/2.dc6b4129.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-coronatracker/static/js/2.dc6b4129.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b9747e988af24490f75",
+    "url": "/react-coronatracker/static/js/main.a3ea419e.chunk.js"
+  },
+  {
+    "revision": "19759d26c5a8b68e0655",
+    "url": "/react-coronatracker/static/js/runtime-main.e1b9c4b5.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-coronatracker/static/media/image.d7265326.png"
+  }
+]);
